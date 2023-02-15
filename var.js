ab8896f747228acd631340933d75276727f1aca6
jaziyaashraf <71220653+jaziyaashraf@users.noreply.github.com>
2023-02-15 16:22:06 +0530

    Add files via upload

---
+++ var.js
@@ -0,0 +1,10 @@
+// var age=prompt("enter your age");
+// document.getElementById('someText').innerHTML=age;
+// var num1=10;
+// num1=num1+1;
+// console.log(num1);
+//functions//
+function fun()
+{
+    alert('this is a function');
+}
